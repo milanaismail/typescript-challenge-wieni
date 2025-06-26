5447196ec5dd4e14f29e38d6c1bcf61160e0b308
milanaismail <milana_is@hotmail.com>
2025-06-26 12:43:45 +0200

    fix route

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { server } = await import("./app/api/cocktails/cocktails.json");
+    const { server } = await import("./mocks/server"); // <- not from a JSON file
     server.listen();
   }
 }
