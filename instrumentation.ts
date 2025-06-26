8077ff74e267ac763235e61cd5b82b13ef6b0dca
milanaismail <milana_is@hotmail.com>
2025-06-26 12:47:19 +0200

    fix

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { server } = await import("./mocks/server"); // <- not from a JSON file
-    server.listen();
-  }
-}
